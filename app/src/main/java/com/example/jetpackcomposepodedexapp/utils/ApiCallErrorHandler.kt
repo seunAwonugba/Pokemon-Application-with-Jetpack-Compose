fb255b91ca4c88da03e1f46800b0d12231449bfd
seunawonugba <seun.awonugba@decagon.dev>
2021-12-06 08:42:37 +0100

    implement api call error handler

---
+++ app/src/main/java/com/example/jetpackcomposepodedexapp/utils/ApiCallErrorHandler.kt
@@ -0,0 +1,13 @@
+package com.example.jetpackcomposepodedexapp.utils
+
+sealed class ApiCallErrorHandler<T>(
+    val data : T? = null,
+    val message : String? =  null
+) {
+    //when success, do this
+    class Success<T>(data: T) : ApiCallErrorHandler<T>(data,null)
+    //when error, get the error message
+    class Error<T>(message: String) : ApiCallErrorHandler<T>(null, message)
+
+    class Loading<T> : ApiCallErrorHandler<T>()
+}
