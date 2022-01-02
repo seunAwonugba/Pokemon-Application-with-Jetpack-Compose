2eadda77e244bdd2e60dc9406d4c0a48798e6810
seunawonugba <seun.awonugba@decagon.dev>
2022-01-02 15:14:10 +0100

    implement loading state and fetching data from remote logic

--- app/src/main/java/com/example/jetpackcomposepodedexapp/constant/Constants.kt
+++ app/src/main/java/com/example/jetpackcomposepodedexapp/constant/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val LIST_END_POINT = "pokemon"
     const val DETAILS_END_POINT = "pokemon/{name}"
+    const val PAGE_SIZE = 20
 }
--- app/src/main/java/com/example/jetpackcomposepodedexapp/viewmodels/list/PokemonListViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposepodedexapp/viewmodels/list/PokemonListViewModel.kt
@@ -3,16 +3,24 @@ package com.example.jetpackcomposepodedexapp.viewmodels.list
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.capitalize
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import com.example.jetpackcomposepodedexapp.constant.Constants.PAGE_SIZE
+import com.example.jetpackcomposepodedexapp.dataclass.PokemonListEntry
 import com.example.jetpackcomposepodedexapp.repository.TestRepository
+import com.example.jetpackcomposepodedexapp.utils.ApiCallErrorHandler
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
-    repository: TestRepository
+    private var repository: TestRepository
 ) : ViewModel() {
 
     //calculate dominant color
@@ -27,6 +35,73 @@ class PokemonListViewModel @Inject constructor(
             palette?.dominantSwatch?.rgb?.let {
                 onFinish(Color(it))
 
+            }
+        }
+
+    }
+
+    //implement pagination
+    private var currentPage = 0
+    //using compose state fetch data from api
+
+    //list of pokemons
+    var pokemonList = mutableStateOf<List<PokemonListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    //loads next set of data while making a network call
+    fun loadingHandler(){
+        viewModelScope.launch {
+            // before setting result emit loading state
+            isLoading.value = true
+            //load pokemon, how many do u want to load, PAGE_SIZE, from where do u want to load from, that the offset
+            var result = repository.pokemonList(PAGE_SIZE, currentPage * PAGE_SIZE)
+
+            when(result){
+                is ApiCallErrorHandler.Success -> {
+                    endReached.value = currentPage * PAGE_SIZE >= result.data!!.count
+                    //get each pokemon
+                    val pokemonEntry = result.data!!.results.mapIndexed { index, entry ->
+                        //get pokemon number from the URL
+                        val number = if (entry.url.endsWith("/")){
+                            entry.url.dropLast(1).takeLastWhile {
+                                it.isDigit()
+                            }
+                        }
+                        //just in case URL does not end with a slash
+                        else{
+                            entry.url.takeLastWhile {
+                                it.isDigit()
+                            }
+                        }
+
+                        //right now we can access image of the pokemon because originally
+                        //from the list of pokemons i can access the sprites, so now i get its number
+                        //pass it to data fetched from details then access the image
+
+                        val myCalculatedImageUrl =
+                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png"
+                        //construct the data passed to the PokemonListEntry i created
+                        PokemonListEntry(
+                            entry.name.capitalize(Locale.ROOT),
+                            myCalculatedImageUrl,
+                            number.toInt()
+                        )
+                    }
+
+                    currentPage++
+                    //since its success no load error
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokemonEntry
+
+                }
+                is ApiCallErrorHandler.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+
+                }
             }
         }
 
